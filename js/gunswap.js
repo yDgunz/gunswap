6c85165ba220b4bcca9962e96c05d3227839ca10
yDgunz <eric.c.gunther@gmail.com>
2014-03-03 23:11:29 -0500

    Prop orbits not quite right

--- js/gunswap.js
+++ js/gunswap.js
@@ -1,18 +1,25 @@
 /* --------- */
 /* CONSTANTS */
 /* --------- */
-var LEFT = 0, RIGHT = 1;
+var LEFT = 0, RIGHT = 1, G = -9.8;
+
+var paused = false;
+var renderMode = '3D';
+var camera, scene, renderer;
+var meshes, floor;
+var camTheta = 0, camPhi = .4, camRadius = 5; // camera starting point
+var isMouseDown = false, onMouseDownTheta, onMouseDownPhi, onMouseDownPosition; // helpers for mouse interaction
 
 /* ------- */
 /* HELPERS */
 /* ------- */
 
-Object.prototype.clone = function() {
+Object.prototype.cloneState = function() {
   var newObj = (this instanceof Array) ? [] : {};
   for (i in this) {
     if (i == 'clone') continue;
     if (this[i] && typeof this[i] == "object") {
-      newObj[i] = this[i].clone();
+      newObj[i] = this[i].cloneState();
     } else newObj[i] = this[i]
   } return newObj;
 };
@@ -68,13 +75,53 @@ function printState(state) {
 				str += (state[juggler][hand][beat] == undefined ? 'X' : state[juggler][hand][beat][0]);
 			}
 		}
-		str += ' ';
+		if (juggler < state.length-1) {			
+			str += ' ';
+		}
 	}
 
 	return str;
 }
 
-/* ---------------*/
+//got the camera rotation code from: http://www.mrdoob.com/projects/voxels/#A/
+function onDocumentMouseDown( event ) {
+	isMouseDown = true;
+	onMouseDownTheta = camTheta;
+	onMouseDownPhi = camPhi;
+	onMouseDownPosition.x = event.clientX;
+	onMouseDownPosition.y = event.clientY;
+}
+
+function onDocumentMouseMove( event ) {
+	event.preventDefault();
+	if ( isMouseDown ) {
+		camTheta = - ( ( event.clientX - onMouseDownPosition.x ) * 0.01 ) + onMouseDownTheta;
+		
+		var dy = event.clientY - onMouseDownPosition.y;
+		//TODO: update this so the camera can't cross the pole
+		camPhi = ( ( dy ) * 0.01 ) + onMouseDownPhi;
+	}
+
+	///update camera
+	camera.position.x = camRadius * Math.sin( camTheta ) * Math.cos( camPhi );
+	camera.position.y = camRadius * Math.sin( camPhi );
+	camera.position.z = camRadius * Math.cos( camTheta ) * Math.cos( camPhi );
+
+	camera.lookAt(new THREE.Vector3(0,1,0));	
+
+	renderer.render(scene, camera);
+}
+
+function onDocumentMouseUp( event ) {
+	event.preventDefault();
+	isMouseDown = false;
+}
+
+function onDocumentMouseWheel( event ) {
+	camRadius -= event.wheelDeltaY*.01;
+}
+
+/* -------------- */
 /* SITESWAP CLASS */
 /* -------------- */
 
@@ -160,8 +207,10 @@ function Siteswap(siteswap) {
 			}
 		});
 
-		if((tmp/beatArr.length % 1) == 0) {
+		if((tmp/beatArr.length % 1) == 0 && tmp/beatArr.length > 0) {
 			this.numProps = tmp/beatArr.length;
+		} else {
+			throw 'Invalid pattern - could not determine number of props'
 		}
 
 		/* if there is a valid number of props, start creating the state array */
@@ -195,8 +244,9 @@ function Siteswap(siteswap) {
 				props.push(i);
 			}
 
-			/* initialize the state array */
+			/* initialize the state and prop orbits array */
 			this.states = [];
+			this.propOrbits = [];
 
 			/* initialize current state */
 			var curState = [];
@@ -249,25 +299,44 @@ function Siteswap(siteswap) {
 					/* iterate through the props landing and look for one landing in the hand that this toss is occurring */
 					for (var k = 0; k < propsLanding.length; k++) {
 						if(propsLanding[k].juggler == toss.juggler && propsLanding[k].hand == tossHand) {
-							prop = propsLanding[k].propId;
+							
+							/* if a prop is landing in a hand this is tossing a 0 then invalid siteswap */
+							if (toss.numBeats == 0) {
+								throw 'Invalid pattern, prop landing on 0 toss';
+							}
+
+							prop = propsLanding[k].propId;														
 						}
 					}
 
-					/* if no props landing to be thrown */
-					if (prop == undefined) {
+					/* if no props landing to be thrown, get one from the queue - only if this isn't a 0 toss */
+					if (prop == undefined && toss.numBeats > 0) {
 						prop = props.shift();			
 					} 
 
-					/* if prop is still undefined (ie. there are none left) then we've got an invalid siteswap */
-					if (prop == undefined) {
-						throw 'Invalid';
+					/* if prop is still undefined (ie. there are none left) then we've got an invalid siteswap - only if this isn't a 0 toss */
+					if (prop == undefined && toss.numBeats > 0) {
+						throw 'Invalid pattern, no prop available to toss';
 					}
 
-					if(curState[toss.targetJuggler][catchHand][toss.numBeats-1] == undefined) {
-						curState[toss.targetJuggler][catchHand][toss.numBeats-1] = [prop];
-					} else {
-						curState[toss.targetJuggler][catchHand][toss.numBeats-1].push(prop);
+					/* so long as this isn't a 0 toss, update the current state and append to prop orbits */
+					if (toss.numBeats > 0) {
+						
+						if(!this.propOrbits[prop]) {
+							this.propOrbits[prop] = [];
+						}
+
+						var tmpPropOrbits = this.propOrbits.cloneState();
+						tmpPropOrbits[prop].push({beat: beat, juggler: toss.juggler, hand: tossHand});
+
+						if(curState[toss.targetJuggler][catchHand][toss.numBeats-1] == undefined) {
+							curState[toss.targetJuggler][catchHand][toss.numBeats-1] = [prop];
+						} else {
+							curState[toss.targetJuggler][catchHand][toss.numBeats-1].push(prop);
+						}
+
 					}
+					
 				}
 								
 
@@ -275,14 +344,17 @@ function Siteswap(siteswap) {
 				if (initComplete && beat % this.tossArr.length == 0 && arraysEqual(this.states[0],curState)) {					
 					patternComplete = true;				
 				} else {
-					/* add the current state to the state array */
-					this.states.push(curState.clone());
+					/* add the current state to the state array and update prop orbits */
+					this.states.push(curState.cloneState());
+					this.propOrbits = tmpPropOrbits;
 				}					
 
 				/* if all props have been introduced to pattern and we're at the end of the pattern, init is complete and steady-state pattern truly begins with the next beat */
 				if (props.length == 0 && (beat+1) % this.tossArr.length == 0 && !initComplete) {
 					initComplete = true;
-					this.states = []; /* reset the state */
+					beat = -1;
+					this.states = []; /* reset the states and prop orbits */
+					this.propOrbits = [];
 				}			
 
 				beat++;
@@ -350,9 +422,351 @@ function Siteswap(siteswap) {
 	}
 
 	this.debugStates = function() {
+		$('#states').empty();
+		/*
 		for (var i = 0; i < this.states.length; i++) {
 			$('#states').append(printState(this.states[i]) + '</br>');
 		}
+		*/
+		$('#states').append("<table border='1'>");
+		var tbl = '';
+		/* juggler header */
+		tbl += '<tr>';
+		for (var i = 0; i < this.states[0].length; i++) {
+			tbl += '<td colspan=' + this.states[0][0][0].length*2 + '>Juggler ' + i + '</td>';
+		}
+		tbl += '</tr><tr>';
+		for (var i = 0; i < this.states[0].length; i++) {
+			tbl += '<td colspan=' + this.states[0][0][0].length + '>Left</td><td colspan=' + this.states[0][0][0].length + '>Right</td>';
+		}
+		tbl += '</tr>'
+		for (var state = 0; state < this.states.length; state++) { // states
+			tbl += '<tr>';
+			for (var juggler = 0; juggler < this.states[state].length; juggler++) { // jugglers
+				for (var hand = 0; hand < this.states[state][juggler].length; hand++) { //hands
+					for (var i = 0; i < this.states[state][juggler][hand].length; i++) { //landing schedule
+						if (this.states[state][juggler][hand][i] == undefined) {
+							tbl += '<td>x</td>';
+						} else {
+							tbl += '<td>'
+							for (var prop = 0; prop < this.states[state][juggler][hand][i].length; prop++) {
+								tbl += (this.states[state][juggler][hand][i][prop] + (prop == this.states[state][juggler][hand][i].length-1 ? '' : ', '));
+							}
+						}						
+					}
+				}
+			}
+			tbl += '</tr>';			
+		}
+		$('#states table').append(tbl);
+
+		$('#propOrbits').empty();
+		$('#propOrbits').append("<table border='1'>");
+		tbl = '';
+		for (var prop = 0; prop < this.propOrbits.length; prop++) {
+			tbl += '<tr><td>Prop ' + prop + '</td>';
+			for (var i = 0; i < this.propOrbits[prop].length; i++) {
+				tbl += ('<td>Beat: ' + this.propOrbits[prop][i].beat + ', Hand: ' + this.propOrbits[prop][i].hand + '</td>');
+			}
+			tbl += '</tr>';
+		}
+		$('#propOrbits table').append(tbl);
+
 	}
 
+}
+
+/* ------- */
+/* JUGGLER */
+/* ------- */
+
+function Juggler(config) {
+	this.position = config.position;
+	this.rotation = config.rotation;
+	this.width = config.width;
+	this.dwellPath = config.dwellPath;
+
+	this.interpolateDwellPath = function(hand,t) {
+		/* t from 0 to 1 */
+		var currentRotation = this.dwellPath[hand].catchRotation + (this.dwellPath[hand].tossRotation - this.dwellPath[hand].catchRotation)*t;
+		
+		var dwellPosition = {
+			x: this.dwellPath[hand].radius*Math.cos(currentRotation),
+			y: this.dwellPath[hand].radius*Math.sin(currentRotation),
+			z: 0
+		};
+
+		return {
+			x: this.position.x + ((hand == LEFT ? -1 : 1)*this.width/2+dwellPosition.x)*Math.cos(this.rotation),
+			y: this.position.y + dwellPosition.y,
+			z: this.position.z + ((hand == LEFT ? -1 : 1)*this.width/2+dwellPosition.x)*Math.sin(this.rotation)
+		};
+
+	}
+}
+
+/* ------- */
+/* ON LOAD */
+/* ------- */
+
+s = new Siteswap('5');
+
+s.debugStates();
+
+/* initialize jugglers */
+var jugglers = [];
+for (var i = 0; i < s.numJugglers; i++) {
+	jugglers.push(
+		new Juggler({
+			position: {x:0,y:1,z:i}, 
+			rotation: 0, 
+			width:1,
+			dwellPath: [
+				/* left */
+				{
+					radius: .2,
+					catchRotation: 2*Math.PI,
+					tossRotation: Math.PI
+				},
+				/* right */
+				{
+					radius: .2,
+					catchRotation: Math.PI,
+					tossRotation: 2*Math.PI
+				}
+			]
+		})
+	);
+}
+
+/* initialize prop orbits */
+propOrbits = [];
+for (var i = 0; i < s.numProps; i++) {
+	propOrbits.push([]);
+}
+
+/* create prop orbits */
+var numSteps = 1000;
+var beatDuration = .2;
+var dwellRatio = .5;
+
+for (var step = 0; step < numSteps; step++) {
+	
+	var currentBeat = Math.floor(step*s.states.length/numSteps);
+	var currentTime = beatDuration*step*s.states.length/numSteps;
+
+	/* find the current state of each prop */
+	for(var prop = 0; prop < s.numProps; prop++) {
+		
+		var tossJuggler, tossHand, catchJuggler, catchHand, tossBeat, catchBeat;
+		
+		for (var i = 0; i < s.propOrbits[prop].length-1; i++) {
+			if (s.propOrbits[prop][i].beat <= currentBeat && s.propOrbits[prop][i+1].beat > currentBeat) {
+				tossBeat = s.propOrbits[prop][i].beat;
+				tossJuggler = s.propOrbits[prop][i].juggler;
+				tossHand = s.propOrbits[prop][i].hand;
+				catchBeat = s.propOrbits[prop][i+1].beat;
+				catchJuggler = s.propOrbits[prop][i+1].juggler;
+				catchHand = s.propOrbits[prop][i+1].hand;
+			} else if (i == s.propOrbits[prop].length-2) {
+				tossBeat = s.propOrbits[prop][i+1].beat;
+				tossJuggler = s.propOrbits[prop][i+1].juggler;
+				tossHand = s.propOrbits[prop][i+1].hand;
+				catchBeat = s.propOrbits[prop][0].beat;
+				catchJuggler = s.propOrbits[prop][0].juggler;
+				catchHand = s.propOrbits[prop][0].hand;
+			}
+		}
+
+		var tossTime = tossBeat*beatDuration+beatDuration*dwellRatio;
+		var catchTime = catchBeat*beatDuration;
+		if (tossTime > catchTime && catchTime > currentTime) { 
+			tossTime -= (beatDuration*s.states.length);
+		}
+		if (tossTime > catchTime && catchTime < currentTime) {
+			catchTime += (beatDuration*s.states.length);	
+		}
+
+		if (currentTime < tossTime) {
+			/* interpolate dwell path */
+			var t = 1-(tossTime - currentTime)/(beatDuration*dwellRatio);
+			propOrbits[prop].push(jugglers[tossJuggler].interpolateDwellPath(tossHand,t));
+		} else {
+
+			/*
+			calculate position at current time
+
+
+			x(t) = x(0) + v_x*t 
+			v_x = (x(T) - x(0))/T
+
+			y(t) = y(0) + v_y(0)*t + .5*G*t*t
+			v_y = (y(T) - y(0) - .5*G*T*T)/T
+
+			where x(0) is current position, x(T) is target position, and T is the time in the air
+			*/
+
+			var T = catchTime - tossTime;
+			var x0 = jugglers[tossJuggler].interpolateDwellPath(tossHand,1).x;
+			var xT = jugglers[catchJuggler].interpolateDwellPath(catchHand,0).x;
+			var y0 = jugglers[tossJuggler].interpolateDwellPath(tossHand,1).y;
+			var yT = jugglers[catchJuggler].interpolateDwellPath(catchHand,0).y;
+			var z0 = jugglers[tossJuggler].interpolateDwellPath(tossHand,1).z;
+			var zT = jugglers[catchJuggler].interpolateDwellPath(catchHand,0).z;
+
+			var t = currentTime - tossTime;
+
+			var xt = x0 + (xT - x0)/T*t;
+			var zt = z0 + (zT - z0)/T*t;
+			var vy = (yT - y0 - .5*G*T*T)/T;
+			var yt = y0 + vy*t + .5*G*t*t;
+
+			propOrbits[prop].push({x: xt, y: yt, z: zt});
+
+		}
+
+	}
+}
+
+/* -------------- */
+/* ANIMATION INIT */
+/* -------------- */
+
+var $container = $('#canvasContainer');
+var width = 400, height = 400;
+
+if (renderMode == '3D') {
+
+	camera = new THREE.PerspectiveCamera( 75, width / height, 1, 10000 );
+	camera.position.x = camRadius * Math.sin( camTheta ) * Math.cos( camPhi );
+	camera.position.y = camRadius * Math.sin( camPhi );
+	camera.position.z = camRadius * Math.cos( camTheta ) * Math.cos( camPhi );
+
+	camera.lookAt(new THREE.Vector3(0,1,0));
+
+	scene = new THREE.Scene();
+
+	/* lights */
+	var pointLight = new THREE.PointLight( 0xffffff );
+	pointLight.position.set(0,4,0);
+	scene.add( pointLight );
+
+	meshes = [];
+
+	/* create each prop */
+	for (var i = 0; i < s.numProps; i++) {
+
+		var mesh = new THREE.Mesh( new THREE.SphereGeometry( .1, 20 ), 
+		new THREE.MeshPhongMaterial( { color: 'red' } ) );
+		mesh.castShadow = true;
+
+		/* synchronize the prop mesh and the prop object */
+		mesh.position.x = 0;
+		mesh.position.y = 0;
+		mesh.position.z = 0;
+
+		mesh.rotation.x = 0;
+		mesh.rotation.y = 0;
+		mesh.rotation.z = 0;
+
+
+		scene.add( mesh );
+		meshes.push( mesh );
+	}
+
+	/* draw floor */
+	var floor = new THREE.Mesh(new THREE.PlaneGeometry(10, 10, 10, 10), new THREE.MeshLambertMaterial( { color: 0xaaaaaa } ));
+	floor.rotation.x += 3*Math.PI/2
+	scene.add(floor);
+
+	var backWall = new THREE.Mesh(new THREE.PlaneGeometry(10, 10, 10, 10), new THREE.MeshLambertMaterial( { color: 0xaaaaaa } ));
+	backWall.position.z -= 1;
+	backWall.position.y += 5;
+	scene.add(backWall);
+
+	/* create the renderer and add it to the canvas container */
+	renderer = new THREE.WebGLRenderer( {antialias: true} );
+	renderer.setSize( width, height );
+
+	$container.empty();
+	$container.append(renderer.domElement);
+
+	//add the event listeners for mouse interaction
+	renderer.domElement.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	renderer.domElement.addEventListener( 'mousedown', onDocumentMouseDown, false );
+	renderer.domElement.addEventListener( 'mouseup', onDocumentMouseUp, false );
+	renderer.domElement.addEventListener( 'mousewheel', onDocumentMouseWheel, false );
+	
+	onMouseDownPosition = new THREE.Vector2();
+
+} else {
+	/* 2D rendering mode */
+
+	$container.empty();
+	$container.append('<canvas id="canvas" width=' + width + ' height=' + height + '></canvas>');
+
+}
+
+var startTime = 0;
+animate();
+
+function animate() {
+	if (startTime == 0) {
+		startTime = (new Date()).getTime();
+	}
+
+	var t = (((new Date()).getTime() - startTime)/1000) % (s.states.length*beatDuration);
+	var step = Math.floor(t/(s.states.length*beatDuration)*1000);
+
+	for (var i = 0; i < meshes.length; i++) {
+		meshes[i].position.x = propOrbits[i][step].x;
+		meshes[i].position.y = propOrbits[i][step].y;
+		meshes[i].position.z = propOrbits[i][step].z;
+	}
+
+	///update camera
+	camera.position.x = camRadius * Math.sin( camTheta ) * Math.cos( camPhi );
+	camera.position.y = camRadius * Math.sin( camPhi );
+	camera.position.z = camRadius * Math.cos( camTheta ) * Math.cos( camPhi );
+
+	camera.lookAt(new THREE.Vector3(0,1,0));	
+
+	try {
+		renderer.render(scene, camera);
+	} catch(e) {
+		console.log('Error rendering');
+	}
+
+	/* update slider position */
+	$('#sliderStep').val( step );
+
+	if (!paused) {
+		requestAnimationFrame(function() { animate(); });
+	}
+
+}
+
+function animateFromSlider() {
+	paused = true;
+
+	var step = $('#sliderStep').val();
+
+	for (var i = 0; i < meshes.length; i++) {
+		meshes[i].position.x = propOrbits[i][step].x;
+		meshes[i].position.y = propOrbits[i][step].y;
+		meshes[i].position.z = propOrbits[i][step].z;
+	}
+
+	///update camera
+	camera.position.x = camRadius * Math.sin( camTheta ) * Math.cos( camPhi );
+	camera.position.y = camRadius * Math.sin( camPhi );
+	camera.position.z = camRadius * Math.cos( camTheta ) * Math.cos( camPhi );
+
+	camera.lookAt(new THREE.Vector3(0,1,0));	
+
+	try {
+		renderer.render(scene, camera);
+	} catch(e) {
+		console.log('Error rendering');
+	}
 }
--- test/test.js
+++ test/test.js
@@ -1,6 +1,11 @@
-/* test that the siteswap object works for a variety of siteswaps. 
-just checks to make sure it doesn't throw an error */
+var eps = .0001; /* margin of error for tests that use Math trig functions */
+
+/* 
+	test that the siteswap object works for a variety of siteswaps. 
+	just checks to make sure it doesn't throw an error 
+*/
 test( "Accepts siteswaps with valid format", function() {
+	
 	new Siteswap('4');
 	new Siteswap('(4,4)');
 	new Siteswap('(4x,4x)(4,4)');
@@ -8,6 +13,7 @@ test( "Accepts siteswaps with valid format", function() {
 	new Siteswap('<3|(4x,4x)>');
   	
   	ok( true );
+
 });
 
 /* test that the siteswap object works for a variety of siteswaps */
@@ -16,7 +22,120 @@ test( "Rejects siteswaps with invalid format", function() {
   throws(
   		function() { new Siteswap('<df>');}
   		, /Invalid/
-  		, "Error message says invalid"
+  		, "Invalid siteswap format"
   	);
+
+});
+
+/* test the max toss height */
+test( "Calculates max toss height correctly", function() {
+	
+	var s = new Siteswap('7531');
+  	ok( s.maxTossHeight == 7 , "7531 has max throw height of 7");
+	s = new Siteswap('(6,4)');
+  	ok( s.maxTossHeight == 3 , "(6,4) has max throw height of 3");
+	s = new Siteswap('<3|(4,4)>');
+  	ok( s.maxTossHeight == 3 , "<3|(4,4)> has max throw height of 3");
+
+});
+
+/* test the number of jugglers */
+test( "Calculates the number of jugglers correctly", function() {
+	
+	var s = new Siteswap('7531');
+  	ok( s.numJugglers == 1 , "7531 has 1 juggler");
+	s = new Siteswap('<3|3>');
+  	ok( s.numJugglers == 2 , "<3|3> has 2 jugglers");
+  	s = new Siteswap('<3|3|3|3>');
+  	ok( s.numJugglers == 4 , "<3|3|3|3> has 4 jugglers");
+
+});
+
+/* test the number of props */
+test( "Calculates the number of props correctly", function() {
+	
+	var s = new Siteswap('7531');
+  	ok( s.numProps == 4 , "7531 has 4 props");
+	s = new Siteswap('(6,4)');
+  	ok( s.numProps == 5 , "(6,4) has 5 props");
+	s = new Siteswap('<3|(4x,4x)|[33]>');
+  	ok( s.numProps == 13 , "<3|(4x,4x)|[33]> has 13 props");
+
+});
+
+/* test the states and print state function */
+test( "Calculates the state array correctly", function() {
+	
+	var s = new Siteswap('3');
+  	ok( printState(s.states[0]) == "J0 L 1X0 R X2X" , "State array correct");
+
+});
+
+/* test the juggler hand position function */
+test("Calculates juggler hand positions correctly", function() {
+
+	var j = new Juggler({
+		position: {x:0,y:0,z:0}, 
+		rotation: 0, 
+		width:1,
+		dwellPath: [
+			/* left */
+			{
+				radius: .1,
+				catchRotation: 2*Math.PI,
+				tossRotation: Math.PI
+			},
+			/* right */
+			{
+				radius: .1,
+				catchRotation: Math.PI,
+				tossRotation: 2*Math.PI
+			}
+		]
+	});
+	ok ( j.handPosition(LEFT).x == -.5 && j.handPosition(LEFT).y == 0 && j.handPosition(LEFT).z == 0, "Left hand correct" )
+	ok ( j.handPosition(RIGHT).x == .5 && j.handPosition(RIGHT).y == 0 && j.handPosition(RIGHT).z == 0, "Right hand correct" )
+	ok ( Math.abs(j.interpolateDwellPath(LEFT,0).x - (-.4)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(RIGHT,0).x - (.4)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(LEFT,.5).x - (-.5)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(RIGHT,.5).x - (.5)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(LEFT,1).x - (-.6)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(RIGHT,1).x - (.6)) < eps, "Dwell path interpolation function correct" )
+
+	ok ( Math.abs(j.interpolateDwellPath(LEFT,0).y - (0)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(RIGHT,0).y - (0)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(LEFT,.5).y - (-.1)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(RIGHT,.5).y - (-.1)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(LEFT,1).y - (0)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(RIGHT,1).y - (0)) < eps, "Dwell path interpolation function correct" )
+
+	ok ( Math.abs(j.interpolateDwellPath(LEFT,0).z - (0)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(RIGHT,0).z - (0)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(LEFT,.5).z - (0)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(RIGHT,.5).z - (0)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(LEFT,1).z - (0)) < eps, "Dwell path interpolation function correct" )
+	ok ( Math.abs(j.interpolateDwellPath(RIGHT,1).z - (0)) < eps, "Dwell path interpolation function correct" )
+
+	var j = new Juggler({
+		position: {x:0,y:0,z:0}, 
+		rotation: Math.PI, 
+		width:1,
+		dwellPath: [
+			/* left */
+			{
+				radius: .1,
+				catchRotation: 2*Math.PI,
+				tossRotation: Math.PI
+			},
+			/* right */
+			{
+				radius: .1,
+				catchRotation: Math.PI,
+				tossRotation: 2*Math.PI
+			}
+		]
+	});
+	ok ( j.handPosition(LEFT).x == .5 && j.handPosition(LEFT).y == 0 && Math.abs(j.handPosition(LEFT).z-0) < eps, "Left hand correct" )
+	ok ( j.handPosition(RIGHT).x == -.5 && j.handPosition(RIGHT).y == 0 && Math.abs(j.handPosition(RIGHT).z-0) < eps, "Right hand correct" )
 
 });
